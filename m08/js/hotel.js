5f1dfd7461ec0c6831f5526f6175f3a7650a1a7e
yuvalamitay08 <89115434+yuvalamitay08@users.noreply.github.com>
2025-05-31 15:59:53 -0700

    Update hotel.js

--- m08/js/hotel.js
+++ m08/js/hotel.js
@@ -1,33 +1,33 @@
 // Create the hotel template
-function Hotel(name, booked, rooms) {
+function Hotel(name, rooms, booked) {
   this.name = name;
-  this.booked = booked;
   this.rooms = rooms;
+  this.booked = booked;
   this.checkAvailability = function () {
     return this.rooms - this.booked;
-};
-  
-// Create hotel objects
-var quay = new Hotel("Quay", 40, 25);
-var park = new Hotel("Park", 120, 77);
-var sunset = new Hotel("Sunset", 86, 10);
+  };
 }
 
-// Updates the rooms value for hotel1 (quay)
-var details1 = quay.name + " rooms: ";
-details1 += quay.checkAvailability();
+// Create hotel objects
+var quayHotel = new Hotel("Quay", 40, 25);
+var parkHotel = new Hotel("Park", 120, 77);
+var sunsetHotel = new Hotel("Sunset", 86, 10);
+
+// Updates the room values for hotel1 (quay)
+var details1 = quayHotel.name + " rooms: ";
+details1 += quayHotel.checkAvailability();
 var elHotel1 = document.getElementById("hotel1");
 elHotel1.textContent = details1;
 
-// Updates the rooms value for hotel2 (park)
-var details2 = park.name + " rooms: ";
-details2 += park.checkAvailability();
+// Updates the room values for hotel2 (park)
+var details2 = parkHotel.name + " rooms: ";
+details2 += parkHotel.checkAvailability();
 var elHotel2 = document.getElementById("hotel2");
 elHotel2.textContent = details2;
 
-// Updates the rooms value for hotel3 (sunset)
-var details3 = sunset.name + " rooms: ";
-details3 += sunset.checkAvailability();
+// Updates the room values for hotel3 (sunset)
+var details3 = sunsetHotel.name + " rooms: ";
+details3 += sunsetHotel.checkAvailability();
 var elHotel3 = document.getElementById("hotel3");
 elHotel3.textContent = details3;
     
